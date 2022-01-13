fc2cd1fad2944df6b9c422e94af2cc1ea4541b03
PaTrond <trond@hauge.app>
2022-01-13 01:38:56 +0100

    Update with future plans

--- pages/api/documents/document/[document_id].ts
+++ pages/api/documents/document/[document_id].ts
@@ -1,10 +1,17 @@
 "use strict";
 
-import {NextApiRequest, NextApiResponse} from 'next';
-import {db} from "../../../../db.js";
+import { NextApiRequest, NextApiResponse } from 'next';
+import { db } from "../../../../db.js";
 
 export default async function getDocumentById(req: NextApiRequest, res: NextApiResponse) {
-    const id = [req.query.document_id];
-    const document = await db("documents").where("document_id", id.toString()); //[req.query.id] doesnt't work. Hence 1 for now.
-    res.json(document);
+    if (req.method === "GET") {
+        const id = [req.query.document_id];
+        const document = await db("documents").where("document_id", id.toString());
+        res.json(document);
+    }
+
+    // Do something else, like updating
+    if (req.method === "PUT") { 
+        
+    } 
 }
