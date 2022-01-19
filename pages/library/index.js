9a07a33067a34dbeef023ab720fce8fb2f509f98
Oliver <olive@DESKTOP-R044BGP>
2022-01-19 19:49:34 +0100

    Started work on library

--- pages/library/index.js
+++ pages/library/index.js
@@ -1,22 +1,24 @@
 'use strict';
 
 import Link from "next/link";
-
+import {Header} from "../../components/header";
 
 export default function Library({list}) {
     return (
-        <div>
-            <p>Page for library!</p>
-            <hr/>
+        <div className="container">
+            {Header()}
             <div>
-                List: {JSON.stringify(list, null, 2)}
+                List:
+                <ul>
+                    {JSON.stringify(list, null, 2)}
+                </ul>
             </div>
         </div>
     );
 }
 
 Library.getInitialProps = async () => {
-    const res = await fetch("http://localhost:3000/api/documents/documents"); //There must be a better way.
+    const res = await fetch("http://localhost:3000/api/documents/documents", {method: "GET"}); //There must be a better way.
     const json = await res.json();
     return {list: json};
 }
