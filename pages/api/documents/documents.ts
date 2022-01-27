6eba3d08d4d68a001ad84139cafda83147cfd944
= <=>
2022-01-27 20:22:53 +0100

    further improvements documents API

--- pages/api/documents/documents.ts
+++ pages/api/documents/documents.ts
@@ -1,27 +1,23 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import {db} from "../../../db.js";
-import {verify} from "jsonwebtoken";
-import Authenticate from "../user/users"
 
 export default async function getDocuments(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === "GET") {
-        const id = 1; // TODO - Need to get member id or member email from request/api
+        const id = 3; // TODO - Need to get member id or member email from request/api
         const adminField = await db.select("admin").from("members").where("member_id", id).first();
         if (adminField) {
             const admin = adminField.admin;
-
-            // TODO - Change what information from documents is actually returned
             if (admin) {
-                const documents = await db("documents");
+                const documents = await db.column("document_id", "document_name").select().from("documents");
                 res.json(documents);
             } else {
-                const documents = await db("documents").where("shared", true).orWhere("owner", id);
+                const documents = await db.column("document_id", "document_name").select().from("documents").where("shared", true).orWhere("owner", id);
                 res.json(documents);
             }
         } else {
-            res.json({authorized: false});
+            res.status(401).send({ error: "Not authenticated" });
         }
     } else {
-        // Something
+        res.status(405).send({ error: "Method not allowed" });
     }
 }
