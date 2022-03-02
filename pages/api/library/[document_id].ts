a4aef24212447c1a8c4d917b6a39311893f0cb7c
= <=>
2022-03-02 19:37:58 +0100

    Completed logic for fetching document information

--- pages/api/library/[document_id].ts
+++ pages/api/library/[document_id].ts
@@ -2,12 +2,38 @@
 
 import { NextApiRequest, NextApiResponse } from 'next';
 import { db } from "../../../db.js";
+import { verify } from "jsonwebtoken";
 
 export default async function getDocumentById(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === "GET") {
         const id = req.query.document_id;
-        const doc = await db("documents").where("document_id", id).first();
-        res.status(200).send(doc.file);
+        const isMetaDataRequest = req.query.isMetaDataRequest;
+
+        let email = "";
+        verify(req.cookies.auth!, process.env.JWT_SECRET, async function (err, decoded) {
+            if (!err && decoded?.memberEmail) email = decoded.memberEmail;
+        });
+        const member = await db("members").where("email",email).first();
+
+        if (isMetaDataRequest) {
+            const docInfo = await db.select("documents.document_name", "documents.shared", "members.first_name", "members.last_name", "organisations.organisation_name")
+            .from("documents")
+            .leftJoin("members", "documents.owner", "members.member_id")
+            .leftJoin("members_organisations", "documents.owner", "members_organisations.member_id")
+            .leftJoin("organisations", "members_organisations.organisation_id", "organisations.organisation_id")
+            .first();
+
+            if (!docInfo.shared && (!member || member.permission !== "admin" || member.member_id !== docInfo.owner)) {
+                res.status(403).json({ message: "User is not authorised to view content" });
+            }
+            else {
+                res.status(200).json(docInfo);
+            }
+        }
+        else {
+            const doc = await db.select("file").from("documents").where("document_id", id).first();
+            res.status(200).send(doc.file);
+        }
     }
     else {
         res.status(405).send({ message: "Method not allowed" });
--- pages/library/[document_id].tsx
+++ pages/library/[document_id].tsx
@@ -2,11 +2,14 @@
 
 import Header from "../../components/header";
 import dynamic from "next/dynamic";
+import { server } from "../../next.config";
 
-export default function DocumentPage({ isCookie }) {
+export default function DocumentPage({ isCookie, docInfo}) {
   const PDFViewer = dynamic(() => import("../../components/pdf-viewer"), {
     ssr: false
   });
+
+  // docInfo should be used to retrieve and display information regarding the document.
 
   return (
     <>
@@ -19,5 +22,12 @@ export default function DocumentPage({ isCookie }) {
 export async function getServerSideProps(context) {
   const cookie = context.req?.headers.cookie;
   const isCookie = cookie ? true : false;
-  return { props: { isCookie } };
+
+  const res = await fetch(`${server}/api/library/${context.query.document_id}?isMetaDataRequest=true`, {
+    method: "GET",
+    headers: { cookie: cookie }
+  });
+  const docInfo = await res.json();
+
+  return { props: { isCookie, docInfo } };
 }
