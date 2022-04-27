5202a58d37b3b888ef5712af68ba327dcec34cad
= <=>
2022-04-27 11:25:57 +0200

    Fixed authentication error in library

--- pages/api/library/[document_id].ts
+++ pages/api/library/[document_id].ts
@@ -1,38 +1,31 @@
 "use strict";
 
 import { NextApiRequest, NextApiResponse } from 'next';
-import { db } from "../../../db.js";
-import { verify } from "jsonwebtoken";
+import { db } from "../../../db";
+import { getMemberClaims } from '../../../utils/server/user';
 
 export default async function getDocumentById(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === "GET") {
-        const doc_id = req.query.document_id;
-        if (!doc_id) {
+        const docID = parseInt(req.query.document_id as string);
+        const cookie = req.headers.cookie;
+        if (!docID) {
             res.status(500).json({ error: "No document id found" })
             return;
         }
 
-        let email = "";
-        verify(req.cookies.auth!, process.env.JWT_SECRET, async function (err, decoded) {
-            if (!err && decoded?.memberEmail) email = decoded.memberEmail;
-        });
-        const member = await db("members").where("email",email).first();
+        const { id, permission } = getMemberClaims(cookie);
 
         const doc = await db.select("documents.document_name", "documents.document_id", "documents.owner", "documents.document_description", "documents.filename", "documents.public","members.first_name", "members.last_name", "organisations.organisation_name")
             .from("documents")
-            .where("documents.document_id", doc_id)
+            .where("documents.document_id", docID)
             .leftJoin("members", "documents.owner", "members.member_id")
             .leftJoin("members_organisations", "documents.owner", "members_organisations.member_id")
             .leftJoin("organisations", "members_organisations.organisation_id", "organisations.organisation_id")
             .first();
 
-        if (!doc) {
-            res.status(404).json({ error: "Document not found" })
-            return;
-        }
-
-        if (!doc.public && (!member || (member.permission !== "admin" && member.member_id !== doc.owner))) {
+        if (!doc?.public && permission !== "admin" && doc?.owner !== id) {
             res.status(403).json({ error: "User is not authorised to view content" });
+            return;
         }
         else {
             res.status(200).json({ doc })
