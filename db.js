41d9c8e049c6a31f276f156a7892b2593be0f0fd
= <=>
2022-05-15 18:42:04 +0200

    Attempt at fixing ssl issue for Aiven database hosting

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 "use strict";
 
 import knex from "knex";
+import { useCA } from "./app.config";
 
 export const db = knex({
   client: "pg",
@@ -9,9 +10,12 @@ export const db = knex({
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   },
   debug: false,
   pool: {min: 1, max: 5},
-  ssl: process.env.NODE_ENV === "production"
 });
--- pages/api/library/documents.ts
+++ pages/api/library/documents.ts
@@ -3,33 +3,39 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { db } from "../../../db";
 import { getMemberClaims } from "../../../utils/server/user";
-import { METHOD_NOT_ALLOWED } from "../../../messages/apiResponse";
+import { INTERNAL_SERVER_ERROR, METHOD_NOT_ALLOWED } from "../../../messages/apiResponse";
 
 export default async function getDocuments(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === "GET") {
         const cookie = req.cookies.auth;
         const { id, permission } = getMemberClaims(cookie);
 
-        if (permission === "admin") {
-            const documents = await db.select("documents.document_name", "documents.document_description", "documents.document_id", "members.first_name", "members.last_name", "organisations.organisation_name")
-            .from("documents")
-            .leftJoin("members", "documents.owner", "members.member_id")
-            .leftJoin("members_organisations", "documents.owner", "members_organisations.member_id")
-            .leftJoin("organisations", "members_organisations.organisation_id", "organisations.organisation_id")
-            .distinctOn("documents.document_name")
-            .orderBy("documents.document_name", "asc");
-            res.status(200).json(documents);
+        try {
+            if (permission === "admin") {
+                const documents = await db.select("documents.document_name", "documents.document_description", "documents.document_id", "members.first_name", "members.last_name", "organisations.organisation_name")
+                .from("documents")
+                .leftJoin("members", "documents.owner", "members.member_id")
+                .leftJoin("members_organisations", "documents.owner", "members_organisations.member_id")
+                .leftJoin("organisations", "members_organisations.organisation_id", "organisations.organisation_id")
+                .distinctOn("documents.document_name")
+                .orderBy("documents.document_name", "asc");
+                res.status(200).json(documents);
+            }
+            else {
+                const documents = await db.select("documents.document_name", "documents.document_description", "documents.document_id", "members.first_name", "members.last_name", "organisations.organisation_name")
+                .from("documents")
+                .leftJoin("members", "documents.owner", "members.member_id")
+                .leftJoin("members_organisations", "documents.owner", "members_organisations.member_id")
+                .leftJoin("organisations", "members_organisations.organisation_id", "organisations.organisation_id")
+                .where("public", true).orWhere("owner", id)
+                .distinctOn("documents.document_name")
+                .orderBy("documents.document_name", "asc");
+                res.status(200).json(documents);
+            }
         }
-        else {
-            const documents = await db.select("documents.document_name", "documents.document_description", "documents.document_id", "members.first_name", "members.last_name", "organisations.organisation_name")
-            .from("documents")
-            .leftJoin("members", "documents.owner", "members.member_id")
-            .leftJoin("members_organisations", "documents.owner", "members_organisations.member_id")
-            .leftJoin("organisations", "members_organisations.organisation_id", "organisations.organisation_id")
-            .where("public", true).orWhere("owner", id)
-            .distinctOn("documents.document_name")
-            .orderBy("documents.document_name", "asc");
-            res.status(200).json(documents);
+        catch (error) {
+            console.error(error);
+            res.status(500).json(INTERNAL_SERVER_ERROR);
         }
     }
     else {
