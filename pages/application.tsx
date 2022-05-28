24a33206dcdd374f7133f86f615b0f2471be8d75
PaTrond <trond@hauge.app>
2022-05-28 15:39:28 +0200

    Fixed VA exit memory error

--- pages/application.tsx
+++ pages/application.tsx
@@ -4,6 +4,7 @@ import { getMemberClaims } from "../utils/server/user";
 import React, {useEffect} from "react";
 import Unity, {UnityContext} from "react-unity-webgl";
 import Header from "../components/header";
+import {useRouter} from "next/router";
 
 const unityContext = new UnityContext({
   loaderUrl: "Build/Unity_Build.loader.js",
@@ -13,7 +14,7 @@ const unityContext = new UnityContext({
 });
 
 export default function Streaming({  permission }) {
-
+  const router = useRouter();
   let perm = 0;
   
     if (permission === "verified") perm = 1;
@@ -31,14 +32,27 @@ export default function Streaming({  permission }) {
         setTimeout(updatePermission, 50)
     });
     return () => unityContext.removeAllEventListeners();
-  }, []);
+  });
+
+  // clears memory when user navigates away from current page
+  useEffect(() => {
+    router.events.on("routeChangeStart", () => {
+      unityContext.unityInstance.Quit();
+    })
+  });
+
+  // not in use before an exit user action is added. Still useful.
+  useEffect(() => {
+    unityContext.on("quitted", () => {
+      unityContext.unityInstance.Quit();
+    });
+  });
 
   return (
     <>
       {Header(permission)}
       <main>
         <Unity unityContext={unityContext} style={{height: "90%", width: "90%", display: "block", margin: "auto"}}/>
-        <button onClick={updatePermission}>Hit me!</button>
       </main>
     </>
   );
